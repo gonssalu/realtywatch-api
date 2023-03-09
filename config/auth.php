c3e11ee1670d3174b38d24547dbee428fbd8fbfd
tgn <trwygon@outlook.pt>
2023-03-09 20:31:04 +0000

    bug fix

--- config/auth.php
+++ config/auth.php
@@ -36,8 +36,12 @@ return [
     */
 
     'guards' => [
-        'web' => [
+        /*'web' => [
             'driver' => 'session',
+            'provider' => 'users',
+        ],*/
+        'api' => [
+            'driver' => 'passport',
             'provider' => 'users',
         ],
     ],
