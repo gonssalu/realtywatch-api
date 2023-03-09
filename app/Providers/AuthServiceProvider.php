d1be62861403d9bcebcb085f1ec5a994764d52a2
tgn <trwygon@outlook.pt>
2023-03-09 20:22:04 +0000

    installed passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -19,8 +19,11 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+        Passport::loadKeysFrom(__DIR__ . '/../Secrets');
+
+        Passport::hashClientSecrets();
     }
 }
