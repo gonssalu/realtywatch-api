99b53ea8ae80ccdc00c039f64f51ea9c0e6efb6e
tgn <trwygon@outlook.pt>
2023-03-10 01:54:11 +0000

    user model start

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $dates = ['updated_at', 'created_at'];
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'remember_token',
+        'photo_url',
+        'blocked',
     ];
 
     /**
@@ -40,5 +44,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'blocked' => 'boolean',
     ];
 }
