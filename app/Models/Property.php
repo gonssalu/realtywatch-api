0ad3e56af25385ad643b57bba02e37811d6b436d
tgn <trwygon@outlook.pt>
2023-03-11 23:49:59 +0000

    Soft Deletes Property

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Property extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
     /**
      * The attributes that are mass assignable.
      *
