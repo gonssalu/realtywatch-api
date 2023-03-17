3a353750ac79a0af954e22f4e0a9056bbceaaed2
Alexandre <alexandrejeronimo2001@gmail.com>
2023-03-17 20:46:03 +0000

    boolean bugfix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,17 +12,16 @@ class UserSeeder extends Seeder
      */
     public function run(): User
     {
-        //Create a regular user
+        // Create a regular user
         $user = User::factory()->create();
-        //Create a user with no profile picture
-        User::factory()->create([
-            'photo_url' => null,
-        ]);
-        //Create a user with a blocked account
-        User::factory()->create([
-            'blocked' => 'true',
-        ]);
-        //Create a soft deleted user
+
+        // Create a user with no profile picture
+        User::factory()->create(['photo_url' => null]);
+
+        // Create a user with a blocked account
+        User::factory()->create(['blocked' => true]);
+
+        // Create a soft deleted user
         User::factory()->trashed()->create();
 
         return $user;
