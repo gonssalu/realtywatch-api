7d1643c84c53f9a392b77cfc4d2e76ef253caa02
tgn <trwygon@outlook.pt>
2023-03-23 02:31:15 +0000

    Media Seeder to scrap for images

--- config/factory/media.php
+++ config/factory/media.php
@@ -6,9 +6,11 @@ return [
             'api' => 'https://api.dicebear.com/5.x/avataaars/png?seed=',
         ],
     ],
-    /*'property' => [
-        'image' => [
-            'api' => 'https://api.dicebear.com/5.x/avataaars/png?seed=',
-        ],
-    ],*/
+    'property' => [
+        'api' => [
+            'images' => 'https://api.pexels.com/v1/search',
+            'videos' => 'https://api.pexels.com/videos/search',
+            'key' => env('PEXELS_API_KEY'),
+        ]
+    ],
 ];
--- database/seeders/MediaSeeder.php
+++ database/seeders/MediaSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
 
 class MediaSeeder extends Seeder
 {
@@ -11,5 +12,77 @@ class MediaSeeder extends Seeder
      */
     public function run(): void
     {
+        $PHOTO_API_URL = config('factory.media.property.api.images');
+        $VIDEO_API_URL = config('factory.media.property.api.videos');
+        $API_KEY = config('factory.media.property.api.key');
+
+        $photoQueries =
+            [
+                'house',
+                'kitchen interior design',
+                'living room',
+                'bathroom',
+                'bedroom',
+                'interior design',
+                'office',
+                'luxury apartment',
+                'luxury house',
+                'blueprint',
+                'garage building',
+                'warehouse building',
+                'land',
+                'retail shop interior design'
+            ];
+
+        $videoQueries =
+            [
+                'real estate',
+            ];
+
+        $this->saveMediaFromApi($PHOTO_API_URL, $API_KEY, 'photos', $photoQueries);
+        $this->saveMediaFromApi($VIDEO_API_URL, $API_KEY, 'videos', $videoQueries);
+    }
+
+    private function getMediaUrlFromRecord($mediaType, $mediaRecord)
+    {
+        switch ($mediaType) {
+            case 'photos':
+                return $mediaRecord['src']['original'];
+            case 'videos':
+                return $mediaRecord['video_files'][0]['link'];
+            default:
+                return null;
+        }
+    }
+
+    private function saveMediaFromApi($theApiUrl, $apiKey, $mediaType, $keywords)
+    {
+        $headers = ['Authorization' => $apiKey];
+
+        foreach ($keywords as $keyword) {
+            $apiUrl = "$theApiUrl?query={$keyword}&per_page=80&page=1";
+
+            // Fetch response from the API
+            $response = Http::withHeaders($headers)->get($apiUrl);
+
+            if ($response->ok()) {
+                $media = $response->json()[$mediaType];
+
+                // Create a directory for the current keyword's media
+                $directory = public_path("$mediaType/{$keyword}");
+                if (!is_dir($directory)) {
+                    mkdir($directory, 0755, true);
+                }
+
+                // Loop through each media and download it
+                foreach ($media as $mediaRecord) {
+                    $srcUrl = $this->getMediaUrlFromRecord($mediaType, $mediaRecord);
+                    $filename = basename($srcUrl);
+                    $filepath = "{$directory}/{$filename}";
+
+                    Http::withOptions(['sink' => $filepath])->get($srcUrl);
+                }
+            }
+        }
     }
 }
