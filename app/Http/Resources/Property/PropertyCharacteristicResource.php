f8b2d23e337ec1cd1fb3f37efa2ab1d8ae9c66f8
tgn <trwygon@outlook.pt>
2023-07-08 21:50:49 +0100

    feat: send characteristic ids

--- app/Http/Resources/Property/PropertyCharacteristicResource.php
+++ app/Http/Resources/Property/PropertyCharacteristicResource.php
@@ -16,6 +16,7 @@ class PropertyCharacteristicResource extends JsonResource
     {
         return
             [
+                'id' => $this->id,
                 'name' => $this->name,
                 'type' => $this->type,
                 'value' => $this->pivot->value,
